03b153b7bc2db7915e5e07fc364cfc97b126e168
Dieferson Weren <dieferson.weren@hotmail.com>
2021-03-21 21:39:14 -0300

    Adding comments to posts

--- src/main/java/com/dief/mongo/config/Instantiation.java
+++ src/main/java/com/dief/mongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.dief.mongo.domain.Post;
 import com.dief.mongo.domain.User;
 import com.dief.mongo.dto.AuthorDto;
+import com.dief.mongo.dto.CommentDto;
 import com.dief.mongo.repository.PostRespository;
 import com.dief.mongo.repository.UserRespository;
 
@@ -40,6 +41,12 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDto(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDto(maria));
 		
+		CommentDto c1 = new CommentDto("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDto(alex));
+		CommentDto c2 = new CommentDto("Aproveite!", sdf.parse("22/03/2018"), new AuthorDto(bob));
+		CommentDto c3 = new CommentDto("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDto(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
