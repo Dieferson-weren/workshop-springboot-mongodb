16a6e54fb12a37ee1a098bf124483f21d34a819b
Dieferson Weren <dieferson.weren@hotmail.com>
2021-03-22 01:37:45 -0300

    Simple query with @Query

--- src/main/java/com/dief/mongo/repository/PostRespository.java
+++ src/main/java/com/dief/mongo/repository/PostRespository.java
@@ -3,11 +3,16 @@ package com.dief.mongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dief.mongo.domain.Post;
 
 @Repository
 public interface PostRespository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
